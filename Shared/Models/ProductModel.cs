19849f05b207ae4e93d9ff80eb195331ec68ed56
Kevin6655 <daitanj65@gmail.com>
2020-01-21 23:33:24 +0900

    [Update]2020/01/21 モデル名の変更　参照の追加

---
+++ Shared/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BElectronApp.Shared.Models
+{
+    /// <summary>
+    /// 商品のproperty
+    /// </summary>
+    public class ProductModel
+    {
+        
+    }
+}
